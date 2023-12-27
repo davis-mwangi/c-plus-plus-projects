c345bbc7d2c14aabad905846c1f5c0a3de3b6b7f
David Mwangi <daudimwash@gmail.com>
2023-12-27 18:08:33 +0300

    add test card  numbers validator

---
+++ testcardnumbers.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+/*
+Luhn algorithm
+1. Double every second digit from right to left, 
+    If doubled number is 2 digits, split them 
+2.Add all single digits from step 1
+3.Add all odd numbered digits from right to left
+4. Sum results from steps 2 & 3 
+5. If step 4 is divisible by 10, then is valid    
+
+*/
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
+
+int main(){
+
+    string cardNumber;
+    int result = 0;
+
+   cout << "Enter a credit Card Number:" << endl;
+   getline(cin, cardNumber);
+
+   result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+   if(result % 10 == 0){
+       cout << cardNumber << " is valid" << endl;
+   }else{
+       cout << cardNumber <<  "is invalid" << endl;
+   }
+
+}
+
+int getDigit(const int number){
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigits(const std::string cardNumber){
+    int sum = 0;
+    for(int i = cardNumber.size() - 1; i >= 0; i-=2){
+          sum +=  getDigit(cardNumber[i] - '0');
+    }
+    return sum;
+}
+int sumEvenDigits(const std::string cardNumber){
+    int sum = 0;
+    for(int i = cardNumber.size() - 2; i >= 0; i-=2){
+          sum +=  getDigit( (cardNumber[i] - '0') * 2);
+    }
+    return sum;
+}
